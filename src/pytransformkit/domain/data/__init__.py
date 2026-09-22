0d0a9055f10c0bda8ef19e0f0ee8acd61bfa94c0
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-22 19:38:10 +0200

    style: sort data model imports

--- src/pytransformkit/domain/data/__init__.py
+++ src/pytransformkit/domain/data/__init__.py
@@ -9,8 +9,8 @@ from pytransformkit.domain.data.data_types import (
     FloatType,
     IntegerType,
     StringType,
-    TimeType,
     TimestampType,
+    TimeType,
     UnknownType,
 )
 from pytransformkit.domain.data.field import Field

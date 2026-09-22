3504cc9989ada5332a65e52f262ec38d1450309d
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-22 19:40:03 +0200

    feat: expose logical dataset model

--- src/pytransformkit/domain/data/__init__.py
+++ src/pytransformkit/domain/data/__init__.py
@@ -13,20 +13,32 @@ from pytransformkit.domain.data.data_types import (
     TimeType,
     UnknownType,
 )
+from pytransformkit.domain.data.dataset import Dataset
 from pytransformkit.domain.data.field import Field
 from pytransformkit.domain.data.field_path import FieldPath
+from pytransformkit.domain.data.metadata import DatasetMetadata
+from pytransformkit.domain.data.references import (
+    DatasetReference,
+    LogicalDatasetReference,
+)
 from pytransformkit.domain.data.schema import Schema
+from pytransformkit.domain.data.statistics import DatasetStatistics
 
 __all__ = [
     "BinaryType",
     "BooleanType",
     "DataType",
+    "Dataset",
+    "DatasetMetadata",
+    "DatasetReference",
+    "DatasetStatistics",
     "DateType",
     "DecimalType",
     "Field",
     "FieldPath",
     "FloatType",
     "IntegerType",
+    "LogicalDatasetReference",
     "Schema",
     "StringType",
     "TimeType",

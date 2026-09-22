4e9b5fa588f87d32b6c35d1d102881b9c8fc2184
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-22 19:35:10 +0200

    feat: initialize logical data model

---
+++ src/pytransformkit/domain/data/__init__.py
@@ -0,0 +1,35 @@
+"""Engine-independent logical data model."""
+
+from pytransformkit.domain.data.data_types import (
+    BinaryType,
+    BooleanType,
+    DataType,
+    DateType,
+    DecimalType,
+    FloatType,
+    IntegerType,
+    StringType,
+    TimeType,
+    TimestampType,
+    UnknownType,
+)
+from pytransformkit.domain.data.field import Field
+from pytransformkit.domain.data.field_path import FieldPath
+from pytransformkit.domain.data.schema import Schema
+
+__all__ = [
+    "BinaryType",
+    "BooleanType",
+    "DataType",
+    "DateType",
+    "DecimalType",
+    "Field",
+    "FieldPath",
+    "FloatType",
+    "IntegerType",
+    "Schema",
+    "StringType",
+    "TimeType",
+    "TimestampType",
+    "UnknownType",
+]
